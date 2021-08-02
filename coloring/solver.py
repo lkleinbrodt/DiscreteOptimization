959fc36cd69c0adc501fa9cbee68b3ce5111e520
Landon Kleinbrodt <lkleinbrodt@gmail.com>
2021-08-02 16:37:06 -0700

    transferring files

--- coloring/solver.py
+++ coloring/solver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+#%%
 from collections import namedtuple
 import numpy as np
 import functools
@@ -22,7 +22,6 @@ class Node:
         self.color = color
         self.neighbors = neighbors
         self.available_colors = None
-        self.degree = self.get_degree()
 
     def get_available_colors(self, possible_colors):
         neighbor_colors = [n.color for n in self.neighbors]
@@ -41,9 +40,9 @@ class Node:
     def append_available_color(self, new_color):
         if self.color is None:
             self.available_colors += [new_color]
-
-    def get_degree(self):
-        return len(self.neighbors)
+    
+    def set_degree(self):
+        self.degree = len(self.neighbors)
 
 def validate_node_list(node_list):
     return all([node.validate_color for node in node_list])
@@ -95,16 +94,15 @@ def unbounded_greedy(node_list):
 
     return node_list
 
+#def bounded_greedy(node_list):
+
+
 def bounded_greedy_prune(node_list):
     """
-    Assumes the nodest already have available colors listed, and tries to fill with those colors
+    Assumes the nodes already have available colors listed, and tries to fill with those colors
     """
-
-    counter = 0
     [n.prune_available_colors() for n in node_list]
     #node_list = sorted(node_list, key = lambda x: -x.degree)
-
-    pick_bottom = True
 
     next_uncolored = next((node for node in node_list if node.color is None), None)
 
@@ -112,21 +110,21 @@ def bounded_greedy_prune(node_list):
 
         try:    
             next_uncolored.color = next_uncolored.available_colors[0]   
-            # if pick_bottom & (len(next_uncolored.available_colors) > 1):
-            #     next_uncolored.color = next_uncolored.available_colors[-1]
-            # else:
-                
-            # pick_bottom = not pick_bottom
-
             
         except IndexError:
             raise MyError('Unable to solve with this color pallete (bounded greedy prune')
 
         [node.prune_available_colors() for node in node_list if node.color is None]
         next_uncolored = next((node for node in node_list if node.color is None), None)
-        counter += 1
 
     return node_list 
+
+def reset_nodes(node_list, max_color=None):
+    for node in node_list:
+        node.color = None
+        if max_color is not None:
+            node.available_colors = node.get_available_colors(possible_colors = list(range(max_color)))
+    return node_list
 
 
 def iterated_dropout(node_list, max_iterations):
@@ -191,12 +189,19 @@ def repeated_greedy_prune(node_list, n_iterations):
     node_list = sorted(node_list, key = lambda x: -x.degree)
     node_list = unbounded_greedy(node_list)
     solved_n = get_max_color(node_list)
+
+    try:
+        clean_node_list = reset_nodes(node_list, solved_n)
+        node_list = bounded_greedy_prune(clean_node_list)
+    except:
+        pass   
+
     best_solution = copy_nodes(node_list)
     best_n = solved_n
+    upper_bound = best_n
 
     ## This just redoes everything and tries again
     i = 0
-    upper_bound = best_n
     while (i < n_iterations/2) & (best_n > 2):
         for node in node_list:
             node.color = None
@@ -204,49 +209,62 @@ def repeated_greedy_prune(node_list, n_iterations):
         try:
             node_list = bounded_greedy_prune(node_list)
             solved_n = get_max_color(node_list)
+
+            if solved_n < best_n:
+                best_solution = copy_nodes(node_list)
+                best_n = solved_n
+                upper_bound = best_n
         except MyError:
             pass
-        
-        if solved_n < best_n:
-            best_solution = copy_nodes(node_list)
-            best_n = solved_n
-            upper_bound = best_n
-            #print(f"New Best! Max color: {upper_bound}")
         i += 1
-
-    #print('NOW DROPPING RANDOM COLORS')
 
     ### Only drop certain colors
     i = 0
     while (i < n_iterations/2) & (best_n > 2):
         colors = list(range(get_max_color(node_list)))
-        n_to_choose = random.randint(2, len(colors)-1)
+        if len(colors) < 3:
+            n_to_choose = 1
+        else:
+            n_to_choose = random.randint(2,3)
+
         chosen_colors = random.sample(colors, n_to_choose)
+
+        # Segregate and sort
+        erased_lists = [[] for _ in range(n_to_choose)]
+        leftovers = []
         for node in node_list:
-            if node.color in chosen_colors:
-                node.color = None
-            node.available_colors = node.get_available_colors(possible_colors = list(range(upper_bound)))
+            matched = False
+            for idx, color in enumerate(chosen_colors):
+                if node.color == color:
+                    node.color = None
+                    node.available_colors = node.get_available_colors(possible_colors = list(range(upper_bound)))
+                    erased_lists[idx].append(node)
+                    matched = True
+                    break
+            if not matched:
+                node.available_colors = node.get_available_colors(possible_colors = list(range(upper_bound)))
+                leftovers.append(node)
+        erased_lists.append(leftovers)
+        node_list = []
+        for l in erased_lists:
+            node_list += sorted(l, key = lambda x: -x.degree)
+
         try:
             node_list = bounded_greedy_prune(node_list)
             solved_n = get_max_color(node_list)
+            if solved_n < best_n:
+                best_solution = copy_nodes(node_list)
+                best_n = solved_n
+                upper_bound = best_n
         except MyError:
             pass
         
-        if solved_n < best_n:
-            best_solution = copy_nodes(node_list)
-            best_n = solved_n
-            upper_bound = best_n
-            #print(f"New Best! Max color: {upper_bound}")
         i += 1
 
     return best_solution
 
-
-def solve_it(input_data):
-    # Modify this code to run your optimization algorithm
-
-    # parse the input
-    lines = input_data.split('\n')
+def generate_node_list(lines):
+    
 
     first_line = lines[0].split()
     node_count = int(first_line[0])
@@ -255,52 +273,34 @@ def solve_it(input_data):
     edges = []
     nodes = [Node(i, None, []) for i in range(node_count)]
 
-    def generate_node_list():
-        nodes = [Node(i, None, []) for i in range(node_count)]
+    nodes = [Node(i, None, []) for i in range(node_count)]
 
-        for i in range(1, edge_count + 1):
-            line = lines[i]
-            parts = line.split()
-            #edges.append((int(parts[0]), int(parts[1])))
+    for i in range(1, edge_count + 1):
+        line = lines[i]
+        parts = line.split()
+        #edges.append((int(parts[0]), int(parts[1])))
 
-            nodes[int(parts[0])].neighbors += [nodes[int(parts[1])]]
-            nodes[int(parts[1])].neighbors += [nodes[int(parts[0])]]
-        return nodes
+        nodes[int(parts[0])].neighbors += [nodes[int(parts[1])]]
+        nodes[int(parts[1])].neighbors += [nodes[int(parts[0])]]
 
-    nodes = generate_node_list()
+    for node in nodes:
+        node.set_degree()
+    
+    return nodes
+
+def solve_it(input_data):
+    # Modify this code to run your optimization algorithm
+    lines = input_data.split('\n')
+    # parse the input
+    nodes = generate_node_list(lines)
 
     if len(nodes) < 250:
-        tries = 1000
-        solved_nodes = repeated_greedy_prune(nodes, tries)
+        tries = 5000
     elif len(nodes) < 1000:
-        tries = 50
-        solved_nodes = repeated_greedy_prune(nodes, tries)
+        tries = 100
     else:
-        solved_nodes = unbounded_greedy(nodes)
-
-    
-    #solved_nodes = iterated_dropout(nodes, max_iterations=1000)
-    # solved_nodes = unbounded_greedy(nodes)
-    # upper_bound = get_max_color(solved_nodes)
-
-    # failed = False
-    # nodes = generate_node_list()
-    # for node in nodes:
-    #     node.available_colors = list(range(upper_bound))
-    
-    
-    # while (not failed) & (upper_bound > 1):
-    #     try: 
-    #         print(f"Trying to find solution with max color: {upper_bound}")
-    #         nodes = generate_node_list()
-    #         for node in nodes:
-    #             node.available_colors = list(range(upper_bound))
-    #         solved_nodes = iterated_dropout(nodes, max_iterations = 500)
-    #         solved_n = get_max_color(solved_nodes)
-    #         upper_bound = min([upper_bound - 1, solved_n])
-    #     except MyError:
-    #         print(f"Failed to find solution with max color: {upper_bound}")
-    #         failed = True
+        tries = 10
+    solved_nodes = repeated_greedy_prune(nodes, tries)
 
     solved_colors = [node.color for node in solved_nodes]
     
@@ -310,7 +310,16 @@ def solve_it(input_data):
 
     return output_data
 
+def load_nodes(file_location):
+    with open(file_location, 'r') as input_data_file:
+        input_data = input_data_file.read()
 
+    lines = input_data.split('\n')
+
+    nodes = generate_node_list(lines)
+
+    return nodes
+#%%
 import sys
 if __name__ == '__main__':
     import sys
